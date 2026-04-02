b249eb9858fe6554c337375510c0fe1ffb77182b
Tommy Watson <tommy.jl.watson@gmail.com>
2026-04-02 07:41:45 -0500

    Update Inventory data insertion
    Added new products to Inventory with types and removed old entries, waiting for design decisions to complete the entries.

--- insert_data_script.sql
+++ insert_data_script.sql
@@ -1,26 +1,14 @@
 INSERT INTO Inventory
-	(PROD_NAME, PROD_QTY, PRICE)
+	(PROD_NAME, PROD_QTY, PRICE, PROD_TYPE)
 	VALUES
-		('Teddy Bear', '15', '10.00'),
-		('Slippers', '10', '15.00'),
-		('Pokemon Card Pack', '20', '5.00'),
-		('Socks', '15', '5.00'),
-		('Laptop', '2', '800.00'),
-		('Chair', '2', '50.00'),
-		('Volleyball', '10', '50.00'),
-		('Blanket', '5', '25.00'),
-		('Pure Columbian Cocaine (.125 oz)', '400', '250.00'),
-		('Fishing Rod', '2', '75.00'),
-		('Laundry Detergent', '5', '20.00'),
-		('Costco Hotdog', '100', '1.50'),
-		('Traffic Cone', '5', '35.00'),
-		('Jon Lehuta Hair', '5', '27.00'),
-		('Backpack', '3', '40.00'),
-		('Calculator', '3', '40.00'),
-		('Tape', '15', '5.00'),
-		('Bathmat', '3', '25.00'),
-		('Mug', '10', '10.00'),
-		('Rice Cooker', '2', '25.00')
+		('Pulp Fiction', '15', '5.00', 'Movie'),
+		('Titanic', '10', '5.00', 'Movie'),
+		('C.H.U.D', '2', '100.00', 'Movie'),
+		('Grand Theft Auto IV', '15', '9.99', 'Game'),
+		('The Sims', '5', '9.99', 'Game'),
+		('Jon Lehuta Documentary', '1', '1000000.00', 'Movie'),
+		('Mario Kart Wii', '10', '9.99', 'Game'),
+		('Avatar', '50', '14.99', 'Movie'),
 ;
 
 INSERT INTO Customers
