c0e17393f0e34c00392bfde4ced8501066214160
sdziamentsyeu <s.dziamentsyeu@gmail.com>
2026-04-29 15:56:15 -0500

    Add files via upload

--- login_page.php
+++ login_page.php
@@ -24,6 +24,30 @@
 			<input type="submit" class="submit-btn"><br>
 		</form>
 		
+		<br>
+		<h1> use this to access main homepage, cart, and order status</h1>
+		<form method="POST" action="login_handler.php"> 
+			<input type="hidden" name="useridx" value="UrMom"><br>
+			<input type="hidden" name="userpass" value="IsFat"><br>
+			<input type="submit" value="Log in as user" class="submit-btn"><br>
+		</form>
+		
+		<h1> use this to access main inventory and order fufilment </h1>
+		<form method="POST" action="login_handler.php"> 
+			<input type="hidden" name="useridx" value="Emp"><br>
+			<input type="hidden" name="userpass" value="Emp"><br>
+			<input type="submit" value="Log in as employee" class="submit-btn"><br>
+		</form>
+		
+		<h1> use this to access order fufilment </h1>
+		<form method="POST" action="login_handler.php"> 
+			<input type="hidden" name="useridx" value="Owner"><br>
+			<input type="hidden" name="userpass" value="Owner"><br>
+			<input type="submit" value="Log in as owner" class="submit-btn"><br>
+		</form>
+		
+		<br>
+		
 		<table class="account-table" cellpadding="20" cellspacing="0">
 			<tr>
 				<th>
